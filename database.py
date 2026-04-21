6e8ae64c42dbf88bd270fba7f5cce9f107810195
slo228555-oss <theleshasdrug@gmail.com>
2026-04-21 11:55:28 +0700

    new file - database.py new pictures

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    sql = """
+    CREATE TABLE IF NOT EXISTS user(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        login VARCHAR(1000) NOT NULL DEFAULT'',
+        password VARCHAR(1000) NOT NULL DEFAULT''
+    )
+    """
+    cursor.execute(sql)
+    sql = """
+    CREATE TABLE IF NOT EXISTS picture(
+        id INTEGER PTIMARY KEY AUTOINCREMENT,
+        picture VARCHAR(1000) NOT NULL DEFAULT'',
+        user_id INTEGER,
+        FOREIGN_KEY(user_id) REFERENCES user(id)
+    )
+    """
+
+if __name__ == "__main__":
+    create_db()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
-import Pillow
+from flask import Flask, render_template, redirect, url_for, reques, PIL
 
 app = Flask(__name__)
 
