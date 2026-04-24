fb85f7bf5d748d9bb62744fa35c5effe2670f36a
slo228555-oss <theleshasdrug@gmail.com>
2026-04-24 12:01:40 +0700

    new functions in database. added render templates in main

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_db():
     conn = sqlite3.connect("todo.db")
@@ -15,11 +16,27 @@ def create_db():
     sql = """
     CREATE TABLE IF NOT EXISTS picture(
         id INTEGER PTIMARY KEY AUTOINCREMENT,
-        picture VARCHAR(1000) NOT NULL DEFAULT'',
+        picture_link VARCHAR(1000) NOT NULL DEFAULT'',
         user_id INTEGER,
         FOREIGN_KEY(user_id) REFERENCES user(id)
     )
     """
+
+def add_user(login, password):
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+    hashed_password = generate_password_hash(password)    
+    
+    cursor.execute('INSERT INTO user(?,?)', (hashed_password,login))
+
+def is_user_exists(login):
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM user WHERE login = ?", (login,))
+    user = cursor.fetchone()
+    
+    return user != None
 
 if __name__ == "__main__":
     create_db()
