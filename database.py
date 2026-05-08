2798eacc4fa7eff42f34355a3ce55ec157c43d11
VPHLOfficial <d57298499@gmail.com>
2026-05-08 11:57:30 +0700

    fix_update_code

--- database.py
+++ database.py
@@ -2,35 +2,40 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_db():
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("CAT_MEMES.db")
     cursor = conn.cursor()
 
     sql = """
-    CREATE TABLE IF NOT EXISTS user(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        login VARCHAR(1000) NOT NULL DEFAULT'',
-        password VARCHAR(1000) NOT NULL DEFAULT''
-    )
-    """
-    cursor.execute(sql)
-    sql = """
-    CREATE TABLE IF NOT EXISTS picture(
-        id INTEGER PTIMARY KEY AUTOINCREMENT,
-        picture_link VARCHAR(1000) NOT NULL DEFAULT'',
-        user_id INTEGER,
-        FOREIGN_KEY(user_id) REFERENCES user(id)
-    )
+        CREATE TABLE IF NOT EXISTS user(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            login VARCHAR(1000) NOT NULL DEFAULT'',
+            password VARCHAR(1000) NOT NULL DEFAULT''
+        )
     """
 
+    cursor.execute(sql)
+    
+    sql = """
+        CREATE TABLE IF NOT EXISTS picture(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            picture_link VARCHAR(1000) NOT NULL DEFAULT'',
+            user_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES user(id)
+        )
+    """
+
+    cursor.execute(sql)
+    conn.commit()
+
 def add_user(login, password):
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("CAT_MEMES.db")
     cursor = conn.cursor()
     hashed_password = generate_password_hash(password)    
     
-    cursor.execute('INSERT INTO user(?,?)', (hashed_password,login))
+    cursor.execute('INSERT INTO user(login,password) VALUES(?,?)', (login, hashed_password))
 
 def is_user_exists(login):
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("CAT_MEMES.db")
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM user WHERE login = ?", (login,))
